cf41cf4efaf2e8670fbeb789ff9271a3bd17a5fc
kkimberly702 <bimby8701@gmail.com>
2022-04-02 14:00:32 -0700

    Sign up feature complete

--- api/views/users.py
+++ api/views/users.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
+from ..serializers.user import UserSerializer
 
 class SignUp(generics.CreateAPIView):
-    # Override the authentication/permissions classes so this endpoint
-    # is not authenticated & we don't need any permissions to access it.
-    authentication_classes = ()
-    permission_classes = ()
-    
-    def post(self, request):
-         print('wahooo')
-         return Response({})
+        # Override the authentication/permissions classes so this endpoint
+        # is not authenticated & we don't need any permissions to access it.
+        authentication_classes = ()
+        permission_classes = ()
+
+        def post(self, request):
+            new_user = UserSerializer(data=request.data)
+            if new_user.is_valid():
+                  # Save the user and send back a response!
+                new_user.save()
+                return Response({ 'user': new_user.data }, status=status.HTTP_201_CREATED)
+            else:
+                return Response(new_user.errors, status=status.HTTP_400_BAD_REQUEST) 
